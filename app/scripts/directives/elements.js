73e2a39896086546a14b0902569c7f73ebf3edaf
nkijak <nkijak@chariotsolutions.com>
2014-02-05 13:26:38 -0500

    Fixed chart rendering on change.  Changed how Speechbubble is updateD

--- app/scripts/directives/elements.js
+++ app/scripts/directives/elements.js
@@ -1,78 +1,102 @@
 var module = angular.module("dgmt.ui.elements", ['dgmt.services']);
 
 module.directive("speechBubble", [function() {
-    return {
-        restrict: 'E',
-        templateUrl: '/templates/speechBubble.html',
-        link: function(scope, element, attrs) {
-           scope.$watch('workout', function(value){
-               try {
-                   scope.text = "Drop and Give Me "+value.reduce(function(acc, i) { return acc + i;}, 0) + "!";
-               } catch (e) {
-                   scope.text = "Beat your face!";
-               }
-           });
-        }
+
+  function link(scope, element, attrs) {
+
+    function updateText(values) {
+      var text;
+      try {
+        text = "Drop and Give Me "+values.reduce(function(acc, i) { return acc + i;}, 0) + "!";
+      } catch (e) {
+        text = "Beat your face!";
+      }
+      element.find("div.speech").text(text);
     }
+
+    scope.$watch('workout', function(values){
+      console.log("speechBubble: workout changed..");
+      updateText(values);
+    });
+
+    updateText();
+  }
+
+  return {
+    restrict: 'E',
+    templateUrl: '/templates/speechBubble.html',
+    link: link,
+    scope: {
+      workout: "=" 
+    }
+  }
 }]);
 
 module.directive("workoutSelect", ["PushupSchedule", function(PushupSchedule){
-    return {
-        restrict: 'E',
-        templateUrl: '/templates/workoutSelect.html',
-        controller: function($scope) {
-            this.setWorkout = function(episode, level) {
-                $scope.workout = PushupSchedule.workoutFor(episode, level);
-                PushupSchedule.currentWorkout = $scope.workout;
-            }
-        },
-        link: function(scope, element, attrs, ctrl) {
-            element.find("select").on("change", function() {
-               ctrl.setWorkout(scope.episode.value, scope.level);
-            });
+  return {
+    restrict: 'E',
+    templateUrl: '/templates/workoutSelect.html',
+    controller: function($scope) {
+      this.setWorkout = function(episode, level) {
 
-            scope.episodes = PushupSchedule.episodes();
-            scope.levels = PushupSchedule.levels();
-            scope.episode = scope.episodes[0];
-            scope.level = "EASY";
-        }
+        $scope.workout = PushupSchedule.workoutFor(episode, level);
+        console.log("set scope workout to "+$scope.workout);
+        PushupSchedule.currentWorkout = $scope.workout;
+      }
+    },
+    link: function(scope, element, attrs, ctrl) {
+      element.find("select").on("change", function() {
+        console.log("changing...");
+        ctrl.setWorkout(scope.episode.value, scope.level);
+      });
+
+      scope.episodes = PushupSchedule.episodes();
+      scope.levels = PushupSchedule.levels();
+      scope.episode = scope.episodes[0];
+      scope.level = "EASY";
     }
+  }
 
 }]);
 
 module.directive("overviewChart", [function() {
-    return {
-        restrict: 'E',
-        template: '<svg class="chart" />',
-		link: function(scope, element, attrs){
-						
-			var h = element.prop("offsetHeight"),
-				w = element.prop("offsetWidth");
-			
-			var y = d3.scale.linear()
-						.domain([0, d3.max(scope.workout)])
-						.range([0, h - 20]);
-			var x = d3.scale.linear()
-						.domain([0, scope.workout.length * 20 + 10])
-						.rangeRound([0, w - 50]);
-			var chart = d3.select(".chart");
-			
-		    var bars = chart.selectAll("rect").data(scope.workout);
-			bars.enter()
-				.append("rect")
-				.attr("x", function(d,i){ return x(i * 20 + 10); })
-				.attr("y", function(d) { return h - y(d);})
-				.attr("height", y)
-				.attr("width", 20);
+  return {
+    restrict: 'E',
+    template: '<svg class="chart" />',
+    link: function(scope, element, attrs){
 
-			scope.$watch('workout', function (value) {
-				bars.data(value)
-					.transition()
-						.duration(1000)
-						.attr("height", y);
-			});
-			
-		}
+      var h = element.prop("offsetHeight"),
+      w = element.prop("offsetWidth");
+
+      var y = d3.scale.linear()
+        .domain([0, d3.max(scope.workout)])
+        .range([0, h - 2 ]);
+      var x = d3.scale.linear()
+        .domain([0, scope.workout.length * 20 + 10])
+        .rangeRound([0, w - 50]);
+      var chart = d3.select(".chart");
+
+      var bars = chart.selectAll("rect").data(scope.workout);
+      bars.enter()
+        .append("rect")
+        .attr("x", function(d,i){ return x(i * 20 + 10); })
+        .attr("y", function(d) { return h - y(d); })
+        .attr("height", y )
+        .attr("width", 20);
+
+      scope.$watch('workout', function (value) {
+        var y = d3.scale.linear()
+          .domain([0, d3.max(scope.workout)])
+          .range([0, h - 2 ]);
+
+        bars.data(value)
+        .transition()
+        .duration(500)
+        .attr("height", y)
+        .attr("y", function(d) { return h - y(d); });
+      });
+
     }
+  }
 }]);
 
