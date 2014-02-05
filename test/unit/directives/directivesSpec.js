886e5aede714ab5e74803dcddf56cf2e512281b4
nkijak <nkijak@chariotsolutions.com>
2014-02-05 17:22:14 -0500

    default message test for speechBubble

--- test/unit/directives/directivesSpec.js
+++ test/unit/directives/directivesSpec.js
@@ -1,21 +1,26 @@
 describe('directives', function() {
   beforeEach(angular.mock.module('dgmt.ui.elements'));
-
+  var rootScope;
+  var element;
 
   describe('speech-bubble', function(){
     beforeEach(angular.mock.module('/templates/speechBubble.html'));
 
+    beforeEach(inject(function($compile, $rootScope) {
+        rootScope = $rootScope;
+        element = $compile("<speech-bubble workout='workout'/>")($rootScope);  
+        $rootScope.$digest();
+    }));
+
+    it('should display default message without workout', function() {
+      expect(element.html()).toContain("Beat your face!");
+    });
+
     it('should display a total count message', function() {
+      rootScope.workout = [1,1,1,1]; 
+      rootScope.$digest();
 
-      inject(function($compile, $rootScope, $templateCache) {
-        var element = $compile("<speech-bubble workout='workout'/>")($rootScope);  
-        $rootScope.$digest();
-
-        $rootScope.workout = [1,1,1,1]; 
-        $rootScope.$digest();
-
-        expect(element.html()).toContain("Drop and Give Me 4!");
-      });
+      expect(element.html()).toContain("Drop and Give Me 4!");
     });
   });
 });
